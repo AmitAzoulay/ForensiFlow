ea84f0cb823491e9fa9df6ef5083cfb4dd410827
AmitAzoulay <amitazoulay1611@gmail.com>
2026-08-03 19:51:52 +0300

    break the packages when installing using pip, don't use env

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e
 # ----- server -----
 cd server # enter the server directory
 
-pip install --break-system-packages -r requirements.txt # install server dependencies
+pip install --break-system-packages --ignore-installed -r requirements.txt # install server dependencies
 pkill -f "server\.py" || true # kill any existing server process if it is running
 nohup python3 server.py >> logs/server.log 2>&1 & # run the server in the background and redirect output to a log file
 echo "Server started in background with PID $!" 
